55558cb0d3f95100e601e6d493a3f41b50dbfcc4
Shane Laymance <shane.laymance@gmail.com>
2017-12-29 11:15:50 -0800

    Minor fix on fish git prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -12,8 +12,8 @@ set __fish_git_prompt_char_dirtystate ''
 set __fish_git_prompt_char_stagedstate ' '
 set __fish_git_prompt_char_untrackedfiles ' '
 set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead ' '
-set __fish_git_prompt_char_upstream_behind ' '
+set __fish_git_prompt_char_upstream_ahead '  '
+set __fish_git_prompt_char_upstream_behind '  '
 
 # Set main prompt
 function fish_prompt
